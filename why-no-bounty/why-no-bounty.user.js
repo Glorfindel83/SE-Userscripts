a151e66ed1c73ea32e115c2b09ce27fbef57ede4
Glorfindel <gerwindegroot@gmail.com>
2017-12-24 12:44:44 +0100

    Why No Bounty: the system already warns you for your own questions which aren't old enough

--- why-no-bounty/why-no-bounty.user.js
+++ why-no-bounty/why-no-bounty.user.js
@@ -24,7 +24,7 @@
   }
 
   // If there's a 'start a bounty' or 'question eligible for bounty in x days' link, we have nothing to do.
-  if ($("a.bounty-link.bounty").length !== 0 || $("a[href='/help/bounty']").length !== 0) {
+  if ($("a.bounty-link.bounty").length !== 0 || document.getElementById("bounty-link bounty") != null) {
     return;
   }
   
