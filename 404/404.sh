4f59fa5024227507ca6ccb4bcf9903b523af5f47
Glorfindel <gerwindegroot@gmail.com>
2018-07-24 14:01:12 +0200

    404 images

---
+++ 404/404.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+regex="https:\\/\\/([a-z]+)\\.stackexchange\\.com\\/"
+while read p; do
+  if [[ $p =~ $regex ]]
+  then
+    name="${BASH_REMATCH[1]}"
+    curl "https://cdn.sstatic.net/Sites/$name/img/404.png" -o "$name.404.png"
+  fi
+done < ~/Dropbox/stack-exchange-sites.txt
