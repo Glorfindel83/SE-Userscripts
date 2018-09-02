e1b538a3edc1f6b2ac484169ffb83232db11b0c2
Glorfindel <gerwindegroot@gmail.com>
2018-09-02 11:39:29 +0200

    HNQ research

---
+++ hnq-research/HNQ-average-time-by-site.sql
@@ -0,0 +1,8 @@
+﻿SELECT site, ROUND(AVG(time), 2) AS "average time", COUNT(*) AS "# of Q"
+  FROM (
+    SELECT site, question, COUNT(*) / 6.0 AS time
+      FROM snapshots
+      GROUP BY site, question
+      ORDER BY COUNT(*) DESC
+  ) AS subquery GROUP BY site
+ORDER BY AVG(time) DESC
---
+++ hnq-research/HNQ-snapshots-by-site.sql
@@ -0,0 +1,5 @@
+﻿SELECT site, ROUND(100.0 * COUNT(*) / (SELECT COUNT(*) FROM snapshots), 2) AS "percentage",
+  COUNT(DISTINCT(question)) AS "# of Q"
+  FROM snapshots
+  GROUP BY site
+  ORDER BY COUNT(*) DESC
