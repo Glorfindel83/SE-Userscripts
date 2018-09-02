4374192e6319278f8a58292f90ed89c67c8ae9f4
Glorfindel <gerwindegroot@gmail.com>
2018-09-02 12:07:52 +0200

    HNQ research results

--- hnq-research/HNQ-snapshots-by-site.sql
+++ hnq-research/HNQ-snapshots-by-site.sql
@@ -1,5 +1,10 @@
-﻿SELECT site, ROUND(100.0 * COUNT(*) / (SELECT COUNT(*) FROM snapshots), 2) AS "percentage",
-  COUNT(DISTINCT(question)) AS "# of Q"
-  FROM snapshots
-  GROUP BY site
-  ORDER BY COUNT(*) DESC
+﻿SELECT temp.site, temp.percentage AS "% of HNQ", temp.questions AS "# of HNQ", t.number_of_questions AS "# of all Q on site",
+  ROUND(100.0 * questions / t.number_of_questions, 2) AS "% of all Q on site"
+  FROM (SELECT site, ROUND(100.0 * COUNT(*) / (SELECT COUNT(*) FROM snapshots), 2) AS "percentage",
+	  COUNT(DISTINCT(question)) AS "questions"
+	  FROM snapshots
+	  GROUP BY site
+	  ORDER BY COUNT(*) DESC
+	  ) AS temp
+  INNER JOIN total_question_count AS t on t.site = temp.site
+  ORDER BY 2 DESC
