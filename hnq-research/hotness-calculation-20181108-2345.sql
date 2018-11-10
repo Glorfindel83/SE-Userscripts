a7be305b21645ff86def9d804f89bcc419e23cd2
Glorfindel <gerwindegroot@gmail.com>
2018-11-10 14:22:14 +0100

    HNQ update

--- hnq-research/hotness-calculation-20181108-2345.sql
+++ hnq-research/hotness-calculation-20181108-2345.sql
@@ -1,14 +1,14 @@
-SELECT q.site, q.question, q.timestamp, q.hotness,
+SELECT q.site, q.question, q.hotness,
   (LEAST(COUNT(a.*), 10) * (q.up_votes - q.down_votes) / 5.0 + SUM(a.up_votes - a.down_votes)) /
   (GREATEST(DATE_PART('day', q.timestamp - qq.createddate) * 24 + 
                  DATE_PART('hour', q.timestamp - qq.createddate) + 1, 6) ^ 1.4) * 
-  CASE q.site WHEN 'workplace' THEN 0.45 WHEN 'softwareengineering' THEN 0.45 WHEN 'stackoverflow' THEN 0.8 ELSE 1 END
+  CASE q.site WHEN 'workplace' THEN 0.55 WHEN 'softwareengineering' THEN 0.55 WHEN 'stackoverflow' THEN 0.2 ELSE 1 END
                  AS calculated_hotness,
   q.hotness / (
   (LEAST(COUNT(a.*), 10) * (q.up_votes - q.down_votes) / 5.0 + SUM(a.up_votes - a.down_votes)) /
   (GREATEST(DATE_PART('day', q.timestamp - qq.createddate) * 24 + 
                  DATE_PART('hour', q.timestamp - qq.createddate) + 1, 6) ^ 1.4) * 
-  CASE q.site WHEN 'workplace' THEN 0.45 WHEN 'softwareengineering' THEN 0.45 WHEN 'stackoverflow' THEN 0.8 ELSE 1 END)
+  CASE q.site WHEN 'workplace' THEN 0.55 WHEN 'softwareengineering' THEN 0.55 WHEN 'stackoverflow' THEN 0.2 ELSE 1 END)
                  AS calculated_traffic_factor,
 
                  q.views, s.visits AS traffic
--- hnq-research/hotness-calculation-history-49243.sql
+++ hnq-research/hotness-calculation-history-49243.sql
@@ -1,9 +1,17 @@
 SELECT q.site, q.question, q.timestamp, q.hotness,
   (LEAST(COUNT(a.*), 10) * (q.up_votes - q.down_votes) / 5.0 + SUM(a.up_votes - a.down_votes)) /
   (GREATEST(DATE_PART('day', q.timestamp - qq.createddate) * 24 + 
-                 DATE_PART('hour', q.timestamp - qq.createddate) + 1, 6) ^ 1.4) AS calculated_hotness,
-                 q.views, s.visits AS traffic
+                 DATE_PART('hour', q.timestamp - qq.createddate) + 1, 6) ^ 1.4) * 
+  CASE q.site WHEN 'workplace' THEN 0.55 WHEN 'softwareengineering' THEN 0.55 WHEN 'stackoverflow' THEN 0.2 ELSE 1 END
+                 AS calculated_hotness,
+  q.hotness / (
+  (LEAST(COUNT(a.*), 10) * (q.up_votes - q.down_votes) / 5.0 + SUM(a.up_votes - a.down_votes)) /
+  (GREATEST(DATE_PART('day', q.timestamp - qq.createddate) * 24 + 
+                 DATE_PART('hour', q.timestamp - qq.createddate) + 1, 6) ^ 1.4) * 
+  CASE q.site WHEN 'workplace' THEN 0.55 WHEN 'softwareengineering' THEN 0.55 WHEN 'stackoverflow' THEN 0.2 ELSE 1 END)
+                 AS calculated_traffic_factor,
 
+                 q.views, s.visits AS traffic
   FROM snapshots AS q INNER JOIN questions AS qq
     ON q.site = qq.site AND q.question = qq.id
   INNER JOIN sites AS s ON q.site = s.name
