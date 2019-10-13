19e0c17bd0e8ddceb4a7496ef2c744dde1072851
Glorfindel <gerwindegroot@gmail.com>
2019-10-13 21:22:10 +0200

    Pronoun Assistant v2.0: works on Q&A sites as well

--- pronoun-assistant/pronoun-assistant.user.js
+++ pronoun-assistant/pronoun-assistant.user.js
@@ -8,10 +8,24 @@
 // @updateURL   https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/pronoun-assistant/pronoun-assistant.user.js
 // @downloadURL https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/pronoun-assistant/pronoun-assistant.user.js
 // @supportURL  https://stackapps.com/questions/8440/pronoun-assistant
-// @version     1.4
+// @version     2.0
 // @match       *://chat.stackexchange.com/rooms/*
 // @match       *://chat.stackoverflow.com/rooms/*
 // @match       *://chat.meta.stackexchange.com/rooms/*
+// @match       *://*.stackexchange.com/questions/*
+// @match       *://*.stackoverflow.com/questions/*
+// @match       *://*.superuser.com/questions/*
+// @match       *://*.serverfault.com/questions/*
+// @match       *://*.askubuntu.com/questions/*
+// @match       *://*.stackapps.com/questions/*
+// @match       *://*.mathoverflow.net/questions/*
+// @exclude     *://*.stackexchange.com/questions/ask
+// @exclude     *://*.stackoverflow.com/questions/ask
+// @exclude     *://*.superuser.com/questions/ask
+// @exclude     *://*.serverfault.com/questions/ask
+// @exclude     *://*.askubuntu.com/questions/ask
+// @exclude     *://*.stackapps.com/questions/ask
+// @exclude     *://*.mathoverflow.net/questions/ask
 // @grant       GM_addStyle
 // @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -19,9 +33,6 @@
 // ==/UserScript==
 
 /* global $, waitForKeyElements */
-
-// NICETOHAVE: on main/meta sites as well; right now, there are very few users
-// who have indicated their pronouns in their main/meta profile.
 
 GM_addStyle(`
 .tiny-signature {
@@ -50,7 +61,7 @@ let allPronouns = [
 ].join("|");
 let pronounListRegex = new RegExp('\\W*((' + allPronouns + ')(\\s*/\\s*(' + allPronouns + '))+)\\W*', 'i');
 let myPronounIsRegex = /(https?:\/\/)?(my\.)?pronoun\.is\/[\w/]+/i;
-let explicitPronounsRegex = /pronouns:\s*([^.\n]*)(\.|\n|$)/im;
+let explicitPronounsRegex = /pronouns:\s*([^.\n)]*)(\.|\n|\)|$)/im;
 
 // Keys:   user IDs
 // Values: either a list of DOM elements (specifically, the anchors to chat profiles)
@@ -59,7 +70,7 @@ var users = {};
 // TODO: cache in local storage
 
 // Adds pronoun information to a user's 'signature' in chat.
-function showPronouns(element, pronouns) {
+function showPronounsForChat(element, pronouns) {
   if (pronouns == "") {
     return;
   }
@@ -68,6 +79,14 @@ function showPronouns(element, pronouns) {
     usernameElement.innerHTML = '<span class="name">' + usernameElement.innerHTML + '</span><br/>'
       + '<span class="pronouns">' + pronouns + '</span>';
   });
+}
+
+//
+function showPronouns(element, pronouns) {
+  if (pronouns == "") {
+    return;
+  }
+  element.after($('<span class="pronouns" style="padding-left: 5px;">' + pronouns + '</span>'));
 }
 
 // Check the user's 'about' in their chat profile for pronoun indicators
@@ -100,6 +119,7 @@ function getPronouns(aboutMe) {
   return "";
 }
 
+// Chat signatures
 waitForKeyElements("a.signature", function(jNode) {
   let userID = jNode.attr("href").split("/users/")[1];
   if (!users.hasOwnProperty(userID)) {
@@ -109,13 +129,36 @@ waitForKeyElements("a.signature", function(jNode) {
     $.get("https://" + location.host + "/users/thumbs/" + userID + "?showUsage=true", function(data) {
       let pronouns = data.user_message == null ? "" : getPronouns(data.user_message);
       users[userID].forEach(function (element) {
+        showPronounsForChat(element, pronouns);
+      });
+      users[userID] = pronouns;
+    });
+  } else if (typeof users[userID] == 'string') {
+    showPronounsForChat(jNode, users[userID]);
+  } else {
+    users[userID].push(jNode);
+  }
+});
+
+// Q&A site signatures
+$("div.user-details > a, a.comment-user").each(function() {
+  let link = $(this).prop("href");
+  let userID = parseInt(link.split("/users/")[1]);
+  if (!users.hasOwnProperty(userID)) {    
+    users[userID] = [];
+    users[userID].push($(this));
+    // Read profile
+    $.get(link + "?tab=profile", function(data) {
+      console.log($(data).find("div.profile-user--bio")[0].innerText);
+      let pronouns = getPronouns($(data).find("div.profile-user--bio")[0].innerText);
+      users[userID].forEach(function (element) {
         showPronouns(element, pronouns);
       });
       users[userID] = pronouns;
     });
   } else if (typeof users[userID] == 'string') {
-    showPronouns(jNode, users[userID]);
+    showPronouns($(this), users[userID]);
   } else {
-    users[userID].push(jNode);
+    users[userID].push($(this));
   }
 });
