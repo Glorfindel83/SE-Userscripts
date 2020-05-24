0b99e3ab023b5686c077aa864745641ec74078ab
Glorfindel <gerwindegroot@gmail.com>
2020-05-24 08:56:24 +0200

    SEDE User ID Autofiller is now SEDE Parameter Autofiller (cache busting)

--- user-id-autofiller/user-id-autofiller.user.js
+++ user-id-autofiller/user-id-autofiller.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name        SEDE User ID Autofiller
+// @name        SEDE Parameter Autofiller
 // @namespace   https://github.com/Glorfindel83/
-// @description Automatically fills SEDE parameters 'AccountId' and 'UserId'
+// @description Automatically fills SEDE parameters 'AccountId' and 'UserId' with corresponding values for your account, and breaks the cache with random values for 'Seed' and 'CacheBreaker'.
 // @updateURL   https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/user-id-autofiller/user-id-autofiller.user.js
 // @downloadURL https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/user-id-autofiller/user-id-autofiller.user.js
 // @author      Glorfindel
-// @version     0.3
+// @version     0.4
 // @match       *://data.stackexchange.com/*/query/*
 // @connect     stackexchange.com
 // @connect     *.stackexchange.com
@@ -26,9 +26,14 @@
 
 /* global $ */
 
+function randomInteger() {
+    return Math.floor(Math.random() * Math.pow(2, 31));
+}
+
 (function() {
     'use strict';
 
+    // Autofill user IDs upon page load
     $('#query-params input').each(function() {
         let input = $(this);
         let name = input.attr('name');
@@ -41,6 +46,7 @@
                     onload: function (data) {
                         let matches = /\/users\/(\d+)\//g.exec(data.finalUrl);
                         input.val(matches[1]);
+                        input.after($('<span><em>your own account ID, filled by the SEDE Parameter Autofiller userscript</em></span>'));
                     }
                 });
                 break;
@@ -73,10 +79,20 @@
                     onload: function (data) {
                         let matches = /\/users\/(\d+)\//g.exec(data.finalUrl);
                         input.val(matches[1]);
+                        input.after($('<span><em>your own user ID, filled by the SEDE Parameter Autofiller userscript</em></span>'));
                     }
                 });
                 break;
             }
+            case 'seed': case 'cachebreaker':
+                input.val(randomInteger());
+                input.after($('<span><em>will be replaced by a random number each time you run the query</em></span>'));
+                break;
         }
+    });
+
+    // Autofill seed/cachebreaker upon running the query
+    $("#runQueryForm").submit(function(event) {
+        $("input[name='seed'], input[name='cachebreaker']").attr("value", randomInteger());
     });
 })();
