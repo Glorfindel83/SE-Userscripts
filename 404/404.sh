79f8f899d048f4c150b717a41eabe72dd0279f50
Glorfindel <gerwindegroot@gmail.com>
2020-10-04 12:31:19 +0200

    404 / Captcha image updates

--- 404/404.sh
+++ 404/404.sh
@@ -5,5 +5,7 @@ while read p; do
   then
     name="${BASH_REMATCH[1]}"
     curl "https://cdn.sstatic.net/Sites/$name/img/404.png" -o "$name.404.png"
+    name="${BASH_REMATCH[1]}meta"
+    curl "https://cdn.sstatic.net/Sites/$name/img/404.png" -o "$name.404.png"
   fi
 done < ~/Dropbox/stack-exchange-sites.txt
