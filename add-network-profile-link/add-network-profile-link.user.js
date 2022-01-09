73f2aac9ff2a02813c4f4d8a132bc3ac466ca690
Glorfindel <gerwindegroot@gmail.com>
2022-01-09 11:17:30 +0100

    Add link to network & chat profile v0.2

--- add-network-profile-link/add-network-profile-link.user.js
+++ add-network-profile-link/add-network-profile-link.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name        Stack Exchange - Add link to network profile
+// @name        Stack Exchange - Add link to network and chat profiles
 // @namespace   https://github.com/Glorfindel83/
-// @description Adds a link to the network profile for all user profile pages (even if they've hidden the current community)
+// @description Adds a link to the network profile for all user profile pages (even if they've hidden the current community) and a link to the chat profile
 // @author      Glorfindel
 // @updateURL   https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/add-network-profile-link/add-network-profile-link.user.js
 // @downloadURL https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/add-network-profile-link/add-network-profile-link.user.js
-// @version     0.1
+// @version     0.2
 // @match       *://*.stackexchange.com/users/*
 // @match       *://*.stackoverflow.com/users/*
 // @match       *://*.superuser.com/users/*
@@ -24,22 +24,42 @@
     return;
 
   StackExchange.ready(function() {
-    // Check if link already present
-    let user = $(".user-show-new");
-    if (user.find("button[aria-controls='profiles-menu']").length > 0 ||
-        user.find("a.s-btn[href^='https://stackexchange.com/users/']").length > 0)
-      return;
-
-    // Find network account ID
+    // Find user IDs
+    let userID = StackExchange.user.options.userId;
     let accountID = StackExchange.user.options.accountId;
     
-    // Add to profile
-    let profile = user[0].children[0];
-    let container = profile.children[profile.children.length - 1];
-    container.innerHTML = '<ul class="flex--item list-reset"><li role="menuitem">\n' +
-      '<a href="https://stackexchange.com/users/' + accountID + '" class="d-flex ai-center ws-nowrap s-btn s-btn__outlined s-btn__muted s-btn__icon s-btn__sm d-flex ai-center">\n' +
-      '<svg aria-hidden="true" class="native mln2 mr2 svg-icon iconLogoSEXxs" width="18" height="18" viewBox="0 0 18 18"><path d="M3 4c0-1.1.9-2 2-2h8a2 2 0 012 2H3Z" fill="#8FD8F7"></path><path d="M15 11H3c0 1.1.9 2 2 2h5v3l3-3a2 2 0 002-2Z" fill="#155397"></path><path fill="#46A2D9" d="M3 5h12v2H3z"></path><path fill="#2D6DB5" d="M3 8h12v2H3z"></path></svg>\n' +
-      'Network profile</a>\n' +
-      '</li></ul>';
+    // Check if link already present
+    let user = $(".user-show-new");
+    var existingButton = user.find("button[aria-controls='profiles-menu']");
+    if (existingButton.length == 0) {
+      existingButton = user.find("a.s-btn[href^='https://stackexchange.com/users/']").parents("ul");
+    }
+    if (existingButton.length == 0) {
+      // Add link to network profile
+      let profile = user[0].children[0];
+      let container = profile.children[profile.children.length - 1];
+      container.innerHTML = '<a class="flex--item s-btn s-btn__outlined s-btn__muted s-btn__icon s-btn__sm" href="https://stackexchange.com/users/' + accountID + '" class="d-flex ai-center ws-nowrap s-btn s-btn__outlined s-btn__muted s-btn__icon s-btn__sm d-flex ai-center">\n' +
+        '<svg aria-hidden="true" class="native mln2 mr2 svg-icon iconLogoSEXxs" width="18" height="18" viewBox="0 0 18 18"><path d="M3 4c0-1.1.9-2 2-2h8a2 2 0 012 2H3Z" fill="#8FD8F7"></path><path d="M15 11H3c0 1.1.9 2 2 2h5v3l3-3a2 2 0 002-2Z" fill="#155397"></path><path fill="#46A2D9" d="M3 5h12v2H3z"></path><path fill="#2D6DB5" d="M3 8h12v2H3z"></path></svg>\n' +
+        'Network profile</a>';
+      existingButton = $(container).children();
+    }
+    
+    // Add link to chat profile
+    // NICETOHAVE: add as part of dropdown menu
+    var chatProfile;
+    switch (location.host) {
+      case 'meta.stackexchange.com':
+        chatProfile = 'https://chat.meta.stackexchange.com/users/' + userID;
+        break;
+      case 'stackoverflow.com':
+      case 'meta.stackoverflow.com':
+        chatProfile = 'https://chat.stackoverflow.com/users/' + userID;
+        break;
+      default:
+        chatProfile = 'https://chat.stackexchange.com/account/' + accountID;
+        break;
+    }
+    existingButton.before('<a class="flex--item s-btn s-btn__outlined s-btn__muted s-btn__icon s-btn__sm" href="' + chatProfile + '" class="d-flex ai-center ws-nowrap s-btn s-btn__outlined s-btn__muted s-btn__icon s-btn__sm d-flex ai-center">\n' +
+        'Chat profile</a>');
   });
 })();
