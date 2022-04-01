dd8cac64d90f05e637d7395323c90ce22638c074
Glorfindel <gerwindegroot@gmail.com>
2022-04-01 08:27:14 +0200

    Saviour of Lost Souls v2.6: support for new top bar design

--- saviour-of-lost-souls/saviour-of-lost-souls.user.js
+++ saviour-of-lost-souls/saviour-of-lost-souls.user.js
@@ -5,7 +5,7 @@
 // @author      Glorfindel
 // @updateURL   https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/saviour-of-lost-souls/saviour-of-lost-souls.user.js
 // @downloadURL https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/saviour-of-lost-souls/saviour-of-lost-souls.user.js
-// @version     2.5.1
+// @version     2.6
 // @match       *://meta.stackexchange.com/*
 // @match       *://meta.stackoverflow.com/*
 // @match       *://softwarerecs.stackexchange.com/*
