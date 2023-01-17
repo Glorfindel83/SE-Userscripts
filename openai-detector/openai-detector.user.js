1a132a19a706c3e8fa850cf7c554727da38d1a8a
Makyen <makyen@users.noreply.github.com>
2023-01-17 10:03:26 -0800

    OpenAI detector v0.9

--- openai-detector/openai-detector.user.js
+++ openai-detector/openai-detector.user.js
@@ -8,7 +8,7 @@
 // @updateURL   https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/openai-detector/openai-detector.user.js
 // @downloadURL https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/openai-detector/openai-detector.user.js
 // @supportURL  https://stackapps.com/questions/9611/openai-detector
-// @version     0.8
+// @version     0.9
 // @match       *://*.askubuntu.com/*
 // @match       *://*.mathoverflow.net/*
 // @match       *://*.serverfault.com/*
