6cd62af4f75c37f744db0b950a2e498198a23e24
PurpleMagick <PurpleMagick@users.noreply.github.com>
2024-04-26 11:51:32 +0300

    Add back background to confirm button
    SE seem to have changed their styling, so the primary button is no longer highlighted. They now use a different class to mark that a button needs a highlight. 
    
    Added the class to the confirm button for consistency with other dialogs (e.g., flag, close)

--- saviour-of-lost-souls/saviour-of-lost-souls.user.js
+++ saviour-of-lost-souls/saviour-of-lost-souls.user.js
@@ -221,7 +221,7 @@ function buttonClicked(question) {
         </div>
         <br/>
         <div class="d-flex gs8 gsx s-modal--footer">
-            <button class="flex--item s-btn s-btn__primary" type="submit" onclick="saviourOfLostSouls.submitDialog();">Confirm</button>
+            <button class="flex--item s-btn s-btn__primary s-btn__filled" type="submit" onclick="saviourOfLostSouls.submitDialog();">Confirm</button>
             <button class="flex--item s-btn js-modal-close" type="button" onclick="saviourOfLostSouls.closeDialog();">Cancel</button>
         </div>
         <button class="s-modal--close s-btn s-btn__muted js-modal-close js-last-tabbable" type="button" aria-label="Close" onclick="saviourOfLostSouls.closeDialog();">
