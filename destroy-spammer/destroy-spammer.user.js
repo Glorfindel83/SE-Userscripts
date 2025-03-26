26aca0b5b2fba296494a1d3cd31096556f678eed
Glorfindel <gerwindegroot@gmail.com>
2025-03-26 18:45:35 +0100

    Destroy Spammer v0.10

--- destroy-spammer/destroy-spammer.user.js
+++ destroy-spammer/destroy-spammer.user.js
@@ -5,7 +5,7 @@
 // @author      Glorfindel
 // @updateURL   https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/destroy-spammer/destroy-spammer.user.js
 // @downloadURL https://raw.githubusercontent.com/Glorfindel83/SE-Userscripts/master/destroy-spammer/destroy-spammer.user.js
-// @version     0.9.2
+// @version     0.10
 // @match       *://*.stackexchange.com/users/*
 // @match       *://*.stackoverflow.com/users/*
 // @match       *://stackoverflow.com/users/*
@@ -22,7 +22,7 @@
 // ==/UserScript==
 (function () {
   'use strict';
-  
+
   // Determine user ID
   var userIDRegex = /\/users\/(\d+)\//g.exec(document.location);
   if (userIDRegex == null)
@@ -36,7 +36,7 @@
   if (moderatorLinkElement.length == 0) // Current user is not a moderator, or wrong tab - no action possible
     return;
   var destroySpammerLinkAdded = false;
-  
+
   // This function will create the 'Destroy spammer' link;
   // this can happen either synchronously or asynchronously (after fetching additional data).
   var createDestroyLink = function (userID) {
@@ -44,7 +44,7 @@
     if (destroySpammerLinkAdded)
       return;
     destroySpammerLinkAdded = true;
-    
+
     var destroyLink = document.createElement('a');
     destroyLink.setAttribute('class', 'grid--cell');
     destroyLink.setAttribute('style', 'padding-right: 16px');
@@ -68,11 +68,11 @@
         });
       }
     };
-    
+
     // Add to document
     moderatorLinkElement.after(destroyLink);
   };
-  
+
   // Check for keywords in spammers' profiles
   $.get(document.location.href.split('?')[0] + "?tab=profile", function (data) {
     if (data.search(/1\D844\D909\D0831/g) != -1) {
@@ -82,20 +82,20 @@
 
   // Check for (deleted) questions and answers
   var questionsPanel = $('#user-panel-questions');
-  var undeletedQuestions = questionsPanel.find('div.p12').not('.bg-red-050').length;
-  var deletedQuestions = questionsPanel.find('div.p12.bg-red-050').length;
+  var undeletedQuestions = questionsPanel.find('div.p12').not('.bg-red-100').length;
+  var deletedQuestions = questionsPanel.find('div.p12.bg-red-100').length;
   if (undeletedQuestions > 0) // User has content - use the dialog instead
     return;
   var answersPanel = $('#user-panel-answers');
-  var undeletedAnswers = answersPanel.find('div.p12').not('.bg-red-050').length;
-  var deletedAnswers = answersPanel.find('div.p12.bg-red-050').length;
+  var undeletedAnswers = answersPanel.find('div.p12').not('.bg-red-100').length;
+  var deletedAnswers = answersPanel.find('div.p12.bg-red-100').length;
   if (undeletedAnswers > 0) // User has content - use the dialog instead
     return;
   if (deletedQuestions + deletedAnswers == 0 && !userNameIsSuspicious) // User has no deleted content - use the dialog instead
     return;
   if (deletedQuestions + deletedAnswers > 4) // User has too much deleted content - use the dialog instead
     return;
-  
+
   // Create Destroy link immediately
   createDestroyLink(userID);
 }) ();
